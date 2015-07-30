dad569c3ec39d6551847774878544ad09482bddb
Mel <Muhammad.Mel@gmail.com>
2015-07-30 14:13:46 -0400

    Polish board and game files.

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 require_relative 'pieces'
 require 'colorize'
 
@@ -8,6 +7,11 @@ class Board
   BLACK_NON_PAWN_ROW = 7
   WHITE_PAWN_ROW = 1
   WHITE_NON_PAWN_ROW = 0
+  PIECES = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+  ERROR_FORMATTING = [background: :light_red]
+  SUCCESS_FORMATTING = [color: :light_white, background: :green]
+  INSTRUCTION_FORMATTING = [:red]
+  IN_CHECK_FORMATTING = [color: :black, background: :light_red ]
 
   def initialize(setup_board = true)
     @board = Array.new(BOARD_DIMENSIONS) { Array.new(BOARD_DIMENSIONS) }
@@ -26,19 +30,21 @@ class Board
   def insert_pawns(color)
     row_idx = color == :black ? BLACK_PAWN_ROW : WHITE_PAWN_ROW
     BOARD_DIMENSIONS.times { |col_idx| Pawn.new(color, self, [row_idx, col_idx]) }
+    nil
   end
 
   def insert_non_pawns(color)
-    pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
     row_idx = color == :black ? BLACK_NON_PAWN_ROW : WHITE_NON_PAWN_ROW
 
-    pieces.each_with_index do |piece_class, col_idx|
+    PIECES.each_with_index do |piece_class, col_idx|
       piece_class.new(color, self, [row_idx, col_idx])
     end
+
+    nil
   end
 
   def add_piece_to_board(piece, pos)
-    raise 'position not empty' unless empty?(pos)
+    raise 'Position not empty'.colorize(*ERROR_FORMATTING) unless empty?(pos)
     self[pos] = piece
   end
 
@@ -51,17 +57,19 @@ class Board
   end
 
   def move(current_color, start_pos, end_pos)
-    raise 'start position is empty' if empty?(start_pos)
+    raise 'Start position is empty'.colorize(*ERROR_FORMATTING) if empty?(start_pos)
 
     piece = self[start_pos]
 
-    # debugger
     if piece.color != current_color
-      raise "invalid move; must move #{current_color}'s piece this turn".colorize(background: :light_red)
+      raise "Invalid entry. Must move #{color} piece."
+        .colorize(*ERROR_FORMATTING)
     elsif !piece.moves.include?(end_pos)
-      raise "invalid move; piece can't move that way".colorize(background: :light_red)
+      raise "Invalid move.  Piece does not move that way."
+        .colorize(*ERROR_FORMATTING)
     elsif !piece.valid_moves.include?(end_pos)
-      raise "invalid move; cannot move into check".colorize(background: :light_red)
+      raise "Invalid move.  Moving into check is not allowed."
+        .colorize(*ERROR_FORMATTING)
     end
 
     move!(start_pos, end_pos)
@@ -100,7 +108,7 @@ class Board
       return piece.pos if piece.color == color && piece.is_a?(King)
     end
 
-    raise 'king peice not found on board'.colorize(background: :light_red)
+    raise 'King peice not found on board'.colorize(*ERROR_FORMATTING)
   end
 
   def dup
@@ -116,68 +124,36 @@ class Board
 
 
   def render_board
-    # # -----DEBUGGING LABELS -----
-    # letters = "   0 1 2 3 4 5 6 7"
-    # # -----DEBUGGING LABELS -----
-
-    # -----NORMAL LABELS -----
     letters = "   A B C D E F G H"
-    # -----NORMAL LABELS -----
-
     rendering = "\n\n\n--- Chess by Mel ---"
     rendering << "\n" + letters.dup
     descending_nums = (1..BOARD_DIMENSIONS).to_a.reverse
+
     descending_nums.each do |col|
-
-      # # -----DEBUGGING LABELS -----
-      # rendering << "\n#{col-1} "
-      # # -----DEBUGGING LABELS -----
-
-      # -----NORMAL LABELS -----
       rendering << "\n#{col} "
-      # -----NORMAL LABELS -----
 
       BOARD_DIMENSIONS.times do |row|
         spot_render = self[[col - 1, row]].nil? ? "  " : " #{self[[col - 1, row]].render}"
         color = (row + col - 1).even? ? :on_cyan : :on_light_white
-
         rendering << spot_render.send(color)
-
       end
 
-      # # -----DEBUGGING LABELS -----
-      # rendering << " #{col}"
-      # # -----DEBUGGING LABELS -----
-
-      # -----NORMAL LABELS -----
       rendering << " #{col}"
-      # -----NORMAL LABELS -----
-
     end
-    # # -----DEBUGGING LABELS -----
-    # rendering << "\n" + "   A B C D E F G H"
-    # # -----DEBUGGING LABELS -----
 
-    # -----NORMAL LABELS -----
     rendering << "\n" + letters
-    # -----NORMAL LABELS -----
-
-
     puts rendering
-    # debugger
   end
 
   def [](pos)
-    raise 'invalid position'.colorize(background: :light_red) unless valid_pos?(pos)
+    raise 'Invalid position'.colorize(*ERROR_FORMATTING) unless valid_pos?(pos)
     row, col = pos
-    # debugger if pos = [1,5]
     @board[row][col]
   end
 
   def []=(pos, piece)
-    raise 'invalid position'.colorize(background: :light_red) unless valid_pos?(pos)
+    raise 'Invalid position'.colorize(*ERROR_FORMATTING) unless valid_pos?(pos)
     row, col = pos
     @board[row][col] = piece
   end
-
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,18 @@
-require 'colorize'
 require_relative 'board'
-require 'byebug'
+require 'colorize'
 
 class Game
   attr_reader :board, :current_player, :players
 
   def initialize
     @board = Board.new
-    @players = {
-      white: HumanPlayer.new(:white),
-      black: HumanPlayer.new(:black)
-    }
-
+    @players = { white: HumanPlayer.new(:white), black: HumanPlayer.new(:black) }
     @current_player = :white
   end
 
   def play
     move_count = 0
+
     loop do
       break if move_count > 3 && board.checkmate?(current_player)
       players[current_player].play_turn(board)
@@ -24,18 +20,29 @@ class Game
       move_count += 1
     end
 
-    puts board.render_board
-    puts "Nice game!"
-    puts"#{current_player.capitalize} has been checkmated."
-    puts"#{[:white, :black].reject{ |color| color == current_player }.join.capitalize} wins!\n\n\n"
-    nil
+    game_over
   end
 
+  def game_over
+    puts board.render_board
+    puts "Nice game!".colorize(*Board::SUCCESS_FORMATTING)
+
+    puts"#{current_player.capitalize} is checkmated."
+      .colorize(*Board::INSTRUCTION_FORMATTING)
+
+    puts"#{[:white, :black]
+      .reject{ |color| color == current_player }.join.capitalize} wins!"
+      .colorize(*Board::INSTRUCTION_FORMATTING)
+
+    puts"\n\n"
+    nil
+  end
 end
 
 class HumanPlayer
   VALID_COL_INPUTS = %w(A B C D E F G H)
   VALID_ROW_INPUTS = [1, 2, 3, 4, 5, 6 ,7, 8]
+
   attr_reader :color
 
   def initialize(color)
@@ -44,32 +51,72 @@ class HumanPlayer
 
   def play_turn(board)
     puts board.render_board
-    font_color = color == :white ? :light_white : :black
-    background = color == :white ? :black : :light_white
-    print "Current player: ".colorize(:red)
-    puts " #{color.capitalize} ".colorize(color: font_color, background: background)
 
-    puts "Enter start position of your move (e.g. 'F2').".colorize(:red)
-    # debugger
-    start_pos = sanitize_user_input(gets.chomp)
-    # puts "\nStart position is #{start_pos}.\nPiece at that spot is #{board[start_pos]} and color is #{board[start_pos].color}\n"
-    # puts "Moves are #{board[start_pos].moves}\n"
+    puts "--#{color.capitalize} king in check--"
+      .colorize(*Board::IN_CHECK_FORMATTING) if board.in_check?(color)
 
-    puts "Enter end position of your move (e.g. 'F3').".colorize(:red)
-    end_pos = sanitize_user_input(gets.chomp)
-    # puts "\nEnd position is #{end_pos}.\n"
+    give_user_instructions
+    start_input, start_pos = get_user_start_pos(board)
+    end_input, end_pos = get_user_end_pos(board, start_input, start_pos)
 
     board.move(color, start_pos, end_pos)
+
+    print "Successfully moved #{board[end_pos].class.to_s.downcase}"
+      .colorize(*Board::SUCCESS_FORMATTING)
+    puts " from #{start_input} to #{end_input}."
+      .colorize(*Board::SUCCESS_FORMATTING)
 
     rescue StandardError => e
       puts "Error: #{e.message}"
       retry
   end
 
+  def player_font_and_background_colors
+    font_color = color == :white ? :light_white : :black
+    background = color == :white ? :black : :light_white
+    formatting = [color: font_color, background: background]
+  end
+
+  def give_user_instructions
+    print "Current player: ".colorize(*Board::INSTRUCTION_FORMATTING)
+    puts " #{color.capitalize} ".colorize(*player_font_and_background_colors)
+    puts "Enter position of piece to move (e.g. 'F2')."
+      .colorize(*Board::INSTRUCTION_FORMATTING)
+    nil
+  end
+
+  def get_user_start_pos(board)
+    start_input = (gets.chomp)
+    start_pos = sanitize_user_input(start_input)
+
+    if board.empty?(start_pos)
+      raise "Invalid start position. Square #{start_input} is empty."
+        .colorize(*Board::ERROR_FORMATTING)
+    elsif board[start_pos].color != color
+      raise "Invalid entry. Must move #{color} piece."
+        .colorize(*Board::ERROR_FORMATTING)
+    end
+
+    [start_input, start_pos]
+  end
+
+  def get_user_end_pos(board, start_input, start_pos)
+    print "Enter destination for #{start_input}'s #{color} "
+      .colorize(*Board::INSTRUCTION_FORMATTING)
+    puts"#{board[start_pos].class.to_s.downcase} (e.g. 'F3')."
+      .colorize(*Board::INSTRUCTION_FORMATTING)
+
+    end_input = gets.chomp
+    end_pos = sanitize_user_input(end_input)
+
+    [end_input, end_pos]
+  end
+
   def sanitize_user_input(input)
     input[0] = input[0].upcase
+
     unless VALID_COL_INPUTS.include?(input[0]) && VALID_ROW_INPUTS.include?(input[1].to_i)
-      raise "Invalid input! Try again.".colorize(background: :light_red)
+      raise "Invalid input! Try again.".colorize(*Board::ERROR_FORMATTING)
     end
 
     translate(input)
